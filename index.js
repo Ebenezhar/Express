2fae5396f5a6d3382dde0e3e191d0e5be6d090d1
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-07-21 23:43:08 +0530

    new changes

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { application } = require('express');
 app.use(express.json())
-app.use(cors({
-    orgin: 'http://localhost:3000'
-}))
-
+app.use(cors({ orgin: 'http://localhost:3000' }))
+const mongodb = require('mongodb');
+const { request } = require('express');
+const mongoClient = mongodb.MongoClient;
+const URL = "mongodb+srv://admin:admin1234@cluster0.u9lsu.mongodb.net/test";
 
 let students = [];
-app.get("/students", function (req, res) {
-    res.json(students);
+
+app.post('/student', async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db('schoolManagement');
+        await db.collection('students').insertOne(req.body);
+        await connection.close();
+        res.json({ message: "File created successfully" })
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.post('/student', function (req, res) {
-    students.push(req.body);
-    req.body.id = students.length;
-    res.json({ message: "File created successfully" })
+app.get("/students", async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db('schoolManagement');
+        const students = await db.collection('students').find().toArray();
+        await connection.close();
+        res.json(students);
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
-app.get('/student/:id', function (req, res) {
-    const id = req.params.id;
-    console.log(id);
-    const student = students.find((student) => student.id == id);
-    console.log(student);
-    res.json(student);
+app.get('/student/:id', async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db('schoolManagement');
+        const student = await db.collection('students').findOne({ _id: mongodb.ObjectId(req.params.id) });
+        await connection.close();
+        res.json(student);
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.put('/student/:id', function (req, res) {
-    let id = req.params.id;
-    let studentIndex = students.findIndex(student => student.id == id);
-    console.log(studentIndex);
-    students[studentIndex].email = req.body.email;
-    students[studentIndex].password = req.body.password;
-    res.json('Success')
+app.put('/student/:id', async function (req, res) {
+    try {
+        const connection=  await mongoClient.connect(URL);
+        const db = connection.db('schoolManagement');
+        delete req.body._id;
+        const student = await db.collection('students')
+        .updateOne({_id:mongodb.ObjectId(req.params.id)},{ $set: req.body});
+        await connection.close();
+        res.json({message: "Successfully updated"})
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.delete('/student/:id', function (req, res) {
-    let id = req.params.id;
-    console.log(id);
-    let studentIndex = students.findIndex(student => student.id == id);
-    students.splice(studentIndex, 1);
-    res.json('Successfully deleted');
+app.delete('/student/:id', async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db('schoolManagement')
+        await db.collection('students').deleteOne({ _id: mongodb.ObjectId(req.params.id) })
+        await connection.close();
+        res.json({message:'successfully deleted'});
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 app.listen(3001)
